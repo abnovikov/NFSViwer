1feda64c5cc5b41bb85a7197a8a4d38775bb99c7
abnovikov <alexei.novikov@software.dell.com>
2017-05-06 18:31:46 -0400

    initial commit of entire project

---
+++ NFSDataSource.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace NFSConverter
+{
+    [XmlRoot(ElementName = "Perf_factor")]
+    public class Perf_factor
+    {
+        [XmlElement(ElementName = "List")]
+        public string List { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Probe")]
+    public class Probe
+    {
+        [XmlElement(ElementName = "Field")]
+        public string Field { get; set; }
+        [XmlElement(ElementName = "Frequencies")]
+        public Frequencies Frequencies { get; set; }
+        [XmlElement(ElementName = "Perf_factor")]
+        public Perf_factor Perf_factor { get; set; }
+    }
+    [XmlRoot(ElementName = "Frequencies")]
+    public class Frequencies
+    {
+        [XmlElement(ElementName = "Unit")]
+        public string Unit { get; set; }
+        [XmlElement(ElementName = "List")]
+        public string List { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Measurement")]
+    public class Measurement
+    {
+        [XmlElement(ElementName = "Format")]
+        public string Format { get; set; }
+        [XmlElement(ElementName = "List")]
+        public string List { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Data")]
+    public class Data
+    {
+        [XmlElement(ElementName = "Coordinates")]
+        public string Coordinates { get; set; }
+        [XmlElement(ElementName = "X0")]
+        public string X0 { get; set; }
+        [XmlElement(ElementName = "Xstep")]
+        public string Xstep { get; set; }
+        [XmlElement(ElementName = "Xmax")]
+        public string Xmax { get; set; }
+        [XmlElement(ElementName = "Y0")]
+        public string Y0 { get; set; }
+        [XmlElement(ElementName = "Ystep")]
+        public string Ystep { get; set; }
+        [XmlElement(ElementName = "Ymax")]
+        public string Ymax { get; set; }
+        [XmlElement(ElementName = "Zstep")]
+        public string Zstep { get; set; }
+        [XmlElement(ElementName = "Zmax")]
+        public string Zmax { get; set; }
+        [XmlElement(ElementName = "Z0")]
+        public string Z0 { get; set; }
+        [XmlElement(ElementName = "Frequencies")]
+        public Frequencies Frequencies { get; set; }
+        [XmlElement(ElementName = "Data_files")]
+        public Frequencies Data_files { get; set; }
+        [XmlElement(ElementName = "Measurement")]
+        public Measurement Measurement { get; set; }
+    }
+
+    [XmlRoot(ElementName = "EmissionScan")]
+    public class EmissionScan
+    {
+        [XmlElement(ElementName = "Nfs_ver")]
+        public string Nfs_ver { get; set; }
+        [XmlElement(ElementName = "Filename")]
+        public string Filename { get; set; }
+        [XmlElement(ElementName = "File_ver")]
+        public string File_ver { get; set; }
+        [XmlElement(ElementName = "Data_source")]
+        public string Data_source { get; set; }
+        [XmlElement(ElementName = "Date")]
+        public DateTime Date { get; set; }
+
+        [XmlElement(ElementName = "Source")]
+        public string Source { get; set; }
+        [XmlElement(ElementName = "Disclaimer")]
+        public string Disclaimer { get; set; }
+        [XmlElement(ElementName = "Copyright")]
+        public string Copyright { get; set; }
+
+        [XmlElement(ElementName = "Probe")]
+        public Probe Probe { get; set; }
+        [XmlElement(ElementName = "Data")]
+        public Data Data { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NFSConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var probe = new Probe()
+            {
+                Field = "H"
+            };
+            var measurement = new Measurement()
+            {
+                List = @"26e-3 29e-3 2e-3 -58 22 -60 35 -59 42 -55 51 ",
+                Format = "ma"
+            };
+            var data = new Data()
+            {
+                Coordinates = "xyzcf",
+                Frequencies = new Frequencies()
+                {
+                    //List = @"100 200 300 400",
+                    //                    A.3 File with field azimuth and zenith orientation
+                    List = @"  26e-3 29e-3 2e-3 0 0 - 58 - 60 - 59 - 55
+                            26e-3 29e-3 2e-3 0 90 - 58 - 60 - 59 - 55
+                            26e-3 29e-3 2e-3 90 90 - 58 - 60 - 59 - 55",
+                    Unit = "MHz"
+                },
+                Measurement = measurement
+            };
+
+            EmissionScan emissionscan = new EmissionScan()
+            {
+                File_ver = "1",
+                Nfs_ver = "1.0",
+                Data_source = "measurement",
+                Date = DateTime.Now,
+                Source = "measurement",
+                Disclaimer = "As is",
+                Copyright = "Alex Ltd",
+                Probe = probe,
+                Data = data,
+                Filename = "MyFile"
+            };
+        XmlSerializer serializer = new XmlSerializer(typeof(EmissionScan));
+            serializer.Serialize(Console.Out, emissionscan);
+            Console.ReadLine();
+
+        }
+    }
+}
